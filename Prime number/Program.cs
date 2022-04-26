0bfc32168ddea7cca2031aaa09a36fa4d4880957
Mjchaurasia9 <92732297+Mjchaurasia9@users.noreply.github.com>
2022-04-27 01:14:32 +0530

    Add files via upload

---
+++ Prime number/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num1, num2, ch, i, j;
+Console.WriteLine("Enter Value to check Prime Or Not");
+Console.Write("Enter first Value:\t");
+num1 = int.Parse(Console.ReadLine());
+Console.Write("Enter Second Value:\t");
+num2 = int.Parse(Console.ReadLine());
+for (i = num1; i <= num2; i++)
+{
+ch = 0;
+for ( j = 2; j <= i/2; j++)
+{
+if (i % j == 0)
+{
+ch++;
+break;
+}
+}
+if (ch == 0)
+{
+Console.Write(i+" ");
+}
+}
